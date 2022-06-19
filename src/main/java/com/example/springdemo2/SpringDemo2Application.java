6b0fbd3db6dcbd16a29a2aea8c43ab481db88397
Bora Guven <guvenbe@gmail.com>
2022-06-19 02:31:06 -0400

    simple app

--- src/main/java/com/example/springdemo2/SpringDemo2Application.java
+++ src/main/java/com/example/springdemo2/SpringDemo2Application.java
@@ -2,12 +2,39 @@ package com.example.springdemo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringDemo2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDemo2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemo2Application.class, args);
+    }
+
+    @GetMapping
+    Customer getCusomer(){
+        return new Customer(1L, "James Bond");
+    }
+
+
+    class Customer {
+        private final Long id;
+        private final String name;
+
+        Customer(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 
 }
