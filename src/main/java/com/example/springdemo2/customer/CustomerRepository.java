ddbd613d9643592d1913c77d09cf80e839f9d649
Bora Guven <guvenbe@gmail.com>
2022-06-24 01:11:51 -0400

    implement to interfaces fake

--- src/main/java/com/example/springdemo2/customer/CustomerRepository.java
+++ src/main/java/com/example/springdemo2/customer/CustomerRepository.java
@@ -1,10 +1,12 @@
 package com.example.springdemo2.customer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 @Component
+@Primary
 public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getcustomers() {
