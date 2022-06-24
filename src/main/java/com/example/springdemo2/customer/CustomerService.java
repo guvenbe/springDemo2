2ef17e97266c7f691b1f5156f8f78ef9665dcd35
Bora Guven <guvenbe@gmail.com>
2022-06-24 01:18:48 -0400

    implement to interfaces todo

--- src/main/java/com/example/springdemo2/customer/CustomerService.java
+++ src/main/java/com/example/springdemo2/customer/CustomerService.java
@@ -10,7 +10,7 @@ public class CustomerService {
 
     private final CustomerRepo customerRepo;
 
-    public CustomerService(@Qualifier("fake") CustomerRepo customerRepo) {
+    public CustomerService(CustomerRepo customerRepo) {
         this.customerRepo = customerRepo;
     }
 
