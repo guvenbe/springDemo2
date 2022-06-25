9e674c8ad19a47c8d53bf72508bdd905521e8c43
Bora Guven <guvenbe@gmail.com>
2022-06-25 12:45:32 -0400

    customers with api v2

--- src/main/java/com/example/springdemo2/customer/CustomerControlerV2.java
+++ src/main/java/com/example/springdemo2/customer/CustomerControlerV2.java
@@ -3,22 +3,26 @@ package com.example.springdemo2.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-@Deprecated
+
 @RestController
-@RequestMapping(path = "api/v1/customer")
-public class CustomerControler {
+@RequestMapping(path = "api/v2/customer")
+public class CustomerControlerV2 {
     private final CustomerService customerService;
 
     @Autowired
-    public CustomerControler(CustomerService customerService) {
+    public CustomerControlerV2(CustomerService customerService) {
         this.customerService = customerService;
     }
 
 
     @GetMapping(value = "all")
-    List<Customer> getCusomers() {
-        return customerService.getCusomer();
+    List<Customer> getCusomer() {
+        return Collections.singletonList(
+                new Customer(0L, "v2", "v2")
+        );
     }
 
     @PostMapping
